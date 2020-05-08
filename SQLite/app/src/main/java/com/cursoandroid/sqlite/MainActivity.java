a2dfd2f8218f73284b5e064404fc33c8f2325dfb
Jaqueline_Araujo <jaquelineluiza4444@gmail.com>
2020-05-08 15:29:40 -0300

    Project SQLite

--- SQLite/app/src/main/java/com/cursoandroid/sqlite/MainActivity.java
+++ SQLite/app/src/main/java/com/cursoandroid/sqlite/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +17,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         SQLiteDatabase bdSQLite = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
-        //TABLS
-        bdSQLite.execSQL("CREATE TABLE IF NOT EXISTS pessoas(nome VARCHAR, idade INT(3))");
+        try {
+            //TABLS
+            bdSQLite.execSQL("CREATE TABLE IF NOT EXISTS pessoas(nome VARCHAR, idade INT(3))");
 
-       //INSERIR DADOS NA TABELA
-        bdSQLite.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Jaqueline', 20)");
-        bdSQLite.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Ana', 19)");
-        bdSQLite.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Maria', 20)");
+            //INSERIR DADOS NA TABELA
+            bdSQLite.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Jaqueline', 20)");
+            bdSQLite.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Ana', 19)");
+            bdSQLite.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Maria', 20)");
 
-        //RECUPERAR DADOS
-        Cursor cursor
+            //RECUPERAR DADOS
+            Cursor cursor = bdSQLite.rawQuery("SELECT nome, idade FROM pessoas", null);
+
+            int indiceColunaNome = cursor.getColumnIndex("nome");
+            int indiceColunaIdade = cursor.getColumnIndex("idade");
+
+            cursor.moveToFirst();
+
+            while ( cursor != null ){
+                Log.i("RESULTADO - nome: ", cursor.getString(indiceColunaNome));
+                Log.i("RESULTADO - idade: ", cursor.getString(indiceColunaIdade));
+                cursor.moveToNext();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
